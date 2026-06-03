fe259198d3a3e3fa0f8ad048c7e330cc6e4885d3
helenoktaa <1123150205@global.ac.id>
2026-06-03 10:16:16 +0700

    feat produk model dan provider dengan markdown pricing

--- lib/features/admin/data/models/produk_model.dart
+++ lib/features/admin/data/models/produk_model.dart
@@ -3,6 +3,9 @@ class ProdukModel {
   final String namaProduk;
   final double hargaModal;
   final double hargaJual;
+   final double? hargaDiskon;   
+  final double? porsenDiskon;  
+  final String? sumberDiskon; 
   final int stok;
   final String idKategori;
   final String? namaKategori;
@@ -15,6 +18,9 @@ class ProdukModel {
     required this.namaProduk,
     required this.hargaModal,
     required this.hargaJual,
+    this.hargaDiskon,
+    this.porsenDiskon,
+    this.sumberDiskon,
     required this.stok,
     required this.idKategori,
     this.kategori,
@@ -22,6 +28,9 @@ class ProdukModel {
     required this.statusProduk,
     required this.expiredDate,
   });
+
+double get hargaTampil => hargaDiskon ?? hargaJual;
+  bool get adaDiskon => hargaDiskon != null && hargaDiskon! < hargaJual;
 
   factory ProdukModel.fromJson(Map<String, dynamic> json) {
     final kategoriJson = json['kategori'] as Map<String, dynamic>?;
@@ -31,6 +40,13 @@ class ProdukModel {
       namaProduk: json['nama_produk']?.toString() ?? '',
       hargaModal: (json['harga_modal'] as num).toDouble(),
       hargaJual: (json['harga_jual'] as num).toDouble(),
+      hargaDiskon: json['harga_diskon'] != null
+          ? (json['harga_diskon'] as num).toDouble()
+          : null,
+      porsenDiskon: json['persen_diskon'] != null
+          ? (json['persen_diskon'] as num).toDouble()
+          : null,
+      sumberDiskon: json['sumber_diskon']?.toString(),
       stok: (json['stok'] as num).toInt(),
       // Coba root dulu, kalau kosong ambil dari nested kategori
       idKategori: json['id_kategori']?.toString().isNotEmpty == true
